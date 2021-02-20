26efacde6add6d3360c62585ac82bb6fa73cde3a
fadilmuh22 <fadilmuh2002@gmail.com>
2021-02-20 16:44:58 +0700

    initial commit

---
+++ app/src/main/java/com/example/pwpbsqlite2/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.pwpbsqlite2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Fragment fragment=new MainFragment();
+        FragmentManager fragmentManager=getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction=fragmentManager.beginTransaction();
+        fragmentTransaction.add(R.id.framecontainer,fragment);
+        fragmentTransaction.commit();
+    }
+}
